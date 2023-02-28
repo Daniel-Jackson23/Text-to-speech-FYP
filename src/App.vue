027ffc1d3e37d454d5e19d0d4675386ebde68f7f
Daniel-Jackson23 <danieljackson104@googlemail.com>
2023-02-28 15:34:19 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'
+import NavBar from "@/components/NavBar.vue";
+</script>
+
+<template>
+  <header>
+    <div>
+      <NavBar />
+    </div>
+  </header>
+
+  <RouterView/>
+</template>
+
+<style scoped>
+
+</style>

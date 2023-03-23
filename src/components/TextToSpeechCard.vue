608b8cf4d59fa16746e49e440f428a54d63ed476
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-03-23 15:14:59 +0000

    adding text to speech card

---
+++ src/components/TextToSpeechCard.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "TextToSpeechCard",
+  isLoading: true,
+  selectedVoice: 0,
+  synt: window.speechSynthesis,
+
+
+}
+</script>
+
+<style scoped>
+
+</style>

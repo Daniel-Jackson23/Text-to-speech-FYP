23ac899e884ae504ffb196095f4a41449f7e101e
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-03-25 15:29:49 +0000

    adding loading npm package and adding web speech api into work

--- src/components/TextToSpeechCard.vue
+++ src/components/TextToSpeechCard.vue
@@ -1,18 +1,140 @@
 <template>
+  <div>
+    <transition name="fade" v-if="isLoading">
+      <PulseLoader></PulseLoader>
+    </transition>
+    <transition name="fade" v-if="!isLoading">
+      <div class="form-container">
+        <form @submit.prevent="reading">
+          <h2>Text to Speech</h2>
+          <div class="form-group" v-if="voiceList.length">
+            <label for="voices">Select a voice</label>
+            <select class="form-control" id="voices" v-model="selectedVoice">
+              <option v-for="(voice, index) in voiceList" :data-lang="voice.lang" :value="index">{{ voice.read }}
+                ({{ voice.lang }})
+              </option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="your-text">Enter your text</label>
+            <input class="form-control" id="your-text" type="text" v-model="read" required/>
+          </div>
+          <button type="submit" class="btn btn-success">Read</button>
+        </form>
 
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+
 export default {
   name: "TextToSpeechCard",
-  isLoading: true,
-  selectedVoice: 0,
-  synt: window.speechSynthesis,
+  components: {
+    PulseLoader
+  },
+  data() {
+    return {
+      isLoading: true,
+      read: '',
+      selectedVoice: 0,
+      synth: window.speechSynthesis,
+      voiceList: [],
+      textSpeech: new window.SpeechSynthesisUtterance()
+    }
+  },
+  mounted: function () {
+    this.voiceList = this.synth.getVoices()
 
+    if (this.voiceList.length) {
+      this.isLoading = false
+    }
 
+    this.synth.onvoiceschanged = () => {
+    }
+    this.voiceList = this.synth.getVoices()
+    setTimeout(() => {
+      this.isLoading = false
+    }, 700)
+    this.listenForSpeechEvents()
+  },
+  methods: {
+    listenForSpeechEvents() {
+      this.textSpeech.onstart = () => {
+        this.isLoading = true
+      }
+      this.textSpeech.onend = () => {
+        this.isLoading = false
+      }
+    },
+    reading() {
+      this.textSpeech.text = `${this.read}`
+
+      this.textSpeech.voice = this.voiceList[this.selectedVoice]
+      this.synth.speak(this.textSpeech)
+    }
+  }
 }
+
 </script>
 
-<style scoped>
+<style>
+// https://github.com/tjFogarty/speech-synthesis
 
+
+body, button, input, optgroup, select, textarea {
+  font-family: 'Open Sans', sans-serif;
+}
+
+.form-container {
+  min-width: 100vw;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  padding: 30px;
+  max-width: 600px;
+  margin: 0 auto;
+  background: #fff;
+  border-radius: 3px;
+  box-shadow: 0 10px 30px 10px rgba(0, 0, 0, 0.1);
+}
+
+.v-spinner {
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(255, 255, 255, 0.8);
+  -webkit-backdrop-filter: blur(4px);
+  backdrop-filter: blur(4px);
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity ease .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.btn-success {
+  background: #43C6AC;
+  border-color: #43C6AC;
+  cursor: pointer;
+}
+
+h1 {
+  margin-bottom: 25px;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <script setup>
+import TextToSpeechCard from "@/components/TextToSpeechCard.vue";
 </script>
 
 <template>
   <main class="container px-5 py-5 mx-auto flex flex-row sm:max-lg:flex-col">
-
+    <TextToSpeechCard/>
   </main>
 </template>

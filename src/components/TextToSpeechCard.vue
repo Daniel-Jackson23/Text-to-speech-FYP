2362803e2e08b91d3a12008b8ffecc4e1649f15d
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-03-26 19:27:17 +0100

    get voice function for voice drop down

--- src/components/TextToSpeechCard.vue
+++ src/components/TextToSpeechCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="">
     <transition name="fade" v-if="isLoading">
       <PulseLoader></PulseLoader>
     </transition>
@@ -10,14 +10,14 @@
           <div class="form-group" v-if="voiceList.length">
             <label for="voices">Select a voice</label>
             <select class="form-control" id="voices" v-model="selectedVoice">
-              <option v-for="(voice, index) in voiceList" :data-lang="voice.lang" :value="index">{{ voice.read }}
+              <option v-for="(voice, index) in voiceList" :data-lang="voice.lang" :value="index">{{ voice.name }}
                 ({{ voice.lang }})
               </option>
             </select>
           </div>
           <div class="form-group">
             <label for="your-text">Enter your text</label>
-            <input class="form-control" id="your-text" type="text" v-model="read" required/>
+            <input class="form-control" id="your-text" type="text" v-model="read" required />
           </div>
           <button type="submit" class="btn btn-success">Read</button>
         </form>
@@ -45,7 +45,7 @@ export default {
       textSpeech: new window.SpeechSynthesisUtterance()
     }
   },
-  mounted: function () {
+  mounted() {
     this.voiceList = this.synth.getVoices()
 
     if (this.voiceList.length) {
@@ -53,13 +53,17 @@ export default {
     }
 
     this.synth.onvoiceschanged = () => {
+      this.voiceList = this.synth.getVoices
+
+
+      this.voiceList = this.synth.getVoices()
+      setTimeout(() => {
+        this.isLoading = false
+      }, 700)
+      this.listenForSpeechEvents()
     }
-    this.voiceList = this.synth.getVoices()
-    setTimeout(() => {
-      this.isLoading = false
-    }, 700)
-    this.listenForSpeechEvents()
   },
+
   methods: {
     listenForSpeechEvents() {
       this.textSpeech.onstart = () => {
@@ -81,60 +85,5 @@ export default {
 </script>
 
 <style>
-// https://github.com/tjFogarty/speech-synthesis
-
-
-body, button, input, optgroup, select, textarea {
-  font-family: 'Open Sans', sans-serif;
-}
-
-.form-container {
-  min-width: 100vw;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-form {
-  padding: 30px;
-  max-width: 600px;
-  margin: 0 auto;
-  background: #fff;
-  border-radius: 3px;
-  box-shadow: 0 10px 30px 10px rgba(0, 0, 0, 0.1);
-}
-
-.v-spinner {
-  position: fixed;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(255, 255, 255, 0.8);
-  -webkit-backdrop-filter: blur(4px);
-  backdrop-filter: blur(4px);
-}
-
-.fade-enter-active, .fade-leave-active {
-  transition: opacity ease .5s;
-}
-
-.fade-enter, .fade-leave-to {
-  opacity: 0;
-}
-
-.btn-success {
-  background: #43C6AC;
-  border-color: #43C6AC;
-  cursor: pointer;
-}
-
-h1 {
-  margin-bottom: 25px;
-}
+/* https://github.com/tjFogarty/speech-synthesis */
 </style>

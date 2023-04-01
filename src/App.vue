fac89dec466cf4a9dd1feb87aafe49705c5aae90
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-04-01 16:59:28 +0100

    removing redundant code

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup>
-import {RouterLink, RouterView} from 'vue-router'
+import {RouterView} from 'vue-router'
 import NavBar from "@/components/NavBar.vue";
 </script>
 
 <template>
-  <div class="max-w-8xl mx-auto max-h-full px-4 sm:px-8 md:px-8 bg-primary-blue dark:bg-slate-800">
-    <header>
-      <NavBar />
-    </header>
-  </div>
-  <RouterView/>
+    <div class="max-w-8xl mx-auto max-h-full px-4 sm:px-8 md:px-8 bg-primary-blue dark:bg-slate-800">
+        <header>
+            <NavBar/>
+        </header>
+    </div>
+    <RouterView/>
 </template>
 
 <style>
-html.dark{
-  color-scheme: dark;
+html.dark {
+    color-scheme: dark;
 }
-body, header{
-  @apply bg-slate-50 text-slate-800 dark:bg-slate-800 dark:text-slate-50
+
+body, header {
+    @apply bg-slate-50 text-slate-800 dark:bg-slate-800 dark:text-slate-50
 }
 </style>

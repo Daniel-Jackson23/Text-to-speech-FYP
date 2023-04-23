3a8a0ce71025aab5f0710a1f3c901524de31827c
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-04-23 21:16:29 +0100

    adding pause button

--- src/components/TextToSpeechCard.vue
+++ src/components/TextToSpeechCard.vue
@@ -1,99 +1,87 @@
 <template>
   <div class="container flex justify-center h-96">
-    <transition name="fade" v-if="isLoading">
+    <!-- <transition name="fade" v-if="isLoading">
       <PulseLoader></PulseLoader>
-    </transition>
-    <transition name="fade" v-if="!isLoading">
-      <div
-        class="max-w-lg p-6 bg-white text-black border border-gray-200 rounded-lg shadow dark:bg-gray-600 dark:border-gray-700 dark:text-white"
-      >
-        <form @submit.prevent="reading" class="">
-          <h2 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Text to Speech
-          </h2>
-          <div class="" v-if="voiceList.length">
-            <div class="pb-6">
-              <label
-                for="voices"
-                class="block mb-2 font-bold text-md font-medium text-black dark:text-white"
-                >Select a voice</label
-              >
-              <select
-                class="bg-gray-50 border border-gray-300 text-black text-md rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                id="voices"
-                v-model="selectedVoice"
-              >
-                <option v-for="(voice, index) in voiceList" :data-lang="voice.lang" :value="index">
-                  {{ voice.name }} ({{ voice.lang }})
-                </option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <label for="your-text" class="block mb-2 text-sm font-medium text-black dark:text-white"
-              >Enter your text</label
+    </transition> -->
+    <!-- <transition name="fade" v-if="!isLoading"> -->
+    <div
+      class="max-w-lg p-6 bg-white text-black border border-gray-200 rounded-lg shadow dark:bg-gray-600 dark:border-gray-700 dark:text-white"
+    >
+      <form @submit.prevent="reading" class="">
+        <h2 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Text to Speech
+        </h2>
+        <div class="" v-if="voiceList.length">
+          <div class="pb-6">
+            <label
+              for="voices"
+              class="block mb-2 font-bold text-md font-medium text-black dark:text-white"
+              >Select a voice</label
             >
-            <textarea
-              class="block p-2.5 w-full text-sm text-black bg-gray-50 rounded-lg border border-blue-300 focus:ring-blue-50 focus:border-blue-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              id="your-text"
-              placeholder="Enter text..."
-              type="text"
-              v-model="read"
-              required
-            />
+            <select
+              class="bg-gray-50 border border-gray-300 text-black text-md rounded-lg focus:ring-blue-100 focus:border-blue-100 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              id="voices"
+              v-model="selectedVoice"
+            >
+              <option v-for="(voice, index) in voiceList" :data-lang="voice.lang" :value="index">
+                {{ voice.name }} ({{ voice.lang }})
+              </option>
+            </select>
           </div>
-          <div id="rangeWrapper" class="flex justify-center py-4">
-            <label for="range"  class="m-1">Pitch</label>
-            <div class="pitchVlaue">
-              <input v-on:change="$emit => (pitchVlaue = pitch.value)" type="range" v-model="pitch" name="range" id="pitch" class="" />
-            </div>
-          </div>
-          <button type="submit" class="btn btn-success">Read</button>
-        </form>
-      </div>
-    </transition>
+        </div>
+        <div class="form-group">
+          <label for="your-text" class="block mb-2 text-sm font-medium text-black dark:text-white"
+            >Enter your text</label
+          >
+          <textarea
+            class="block p-2.5 w-full text-sm text-black bg-gray-50 rounded-lg border border-blue-300 focus:ring-blue-50 focus:border-blue-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            id="your-text"
+            placeholder="Enter text..."
+            type="text"
+            v-model="read"
+            required
+          />
+        </div>
+        <button type="submit" class="btn btn-success">Read</button>
+      
+      <button value="pause" @click="pause" type="button" class="">Pause</button>
+    </form>
+    </div>
+    <!-- </transition> -->
   </div>
 </template>
 
 <script>
 import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
-
 export default {
   name: 'TextToSpeechCard',
   components: {
-    PulseLoader
+    // PulseLoader
   },
   data() {
     return {
-      isLoading: true,
+      isLoading: false,
       read: '',
       selectedVoice: 0,
-      synth: window.speechSynthesis,
+      synth: speechSynthesis,
       voiceList: [],
-      textSpeech: new window.SpeechSynthesisUtterance(),
-      pitch: '',
-      pitchVlaue: ''
+      textSpeech: new window.SpeechSynthesisUtterance()
     }
   },
   mounted() {
     this.voiceList = this.synth.getVoices()
-
     if (this.voiceList.length) {
       this.isLoading = false
     }
-
     this.synth.onvoiceschanged = () => {
       this.voiceList = this.synth.getVoices
-
       this.voiceList = this.synth.getVoices()
       setTimeout(() => {
         this.isLoading = false
-      }, 700)
+      })
       this.listenForSpeechEvents()
     }
-    this.textSpeech.pitch = this.pitch
   },
-
   methods: {
     listenForSpeechEvents() {
       this.textSpeech.onstart = () => {
@@ -105,9 +93,11 @@ export default {
     },
     reading() {
       this.textSpeech.text = `${this.read}`
-
       this.textSpeech.voice = this.voiceList[this.selectedVoice]
       this.synth.speak(this.textSpeech)
+    },
+    pause() {
+      speechSynthesis.pause()
     }
   }
 }

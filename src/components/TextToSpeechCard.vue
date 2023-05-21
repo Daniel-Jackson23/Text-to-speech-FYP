40344aa207fce00d3b696978630a8caf88c0456e
Daniel Jackson <80455667+Daniel-Jackson23@users.noreply.github.com>
2023-05-21 11:33:45 +0100

    adding styling for mobile and desktop breakpoints

--- src/components/TextToSpeechCard.vue
+++ src/components/TextToSpeechCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container flex justify-center h-96">
     <div
-      class="lg:w-full p-6 bg-white text-black border border-gray-200 rounded-lg shadow dark:bg-gray-600 dark:border-gray-700 dark:text-white"
+      class=" lg:w-full md:w-full  p-6 bg-white text-black border border-gray-200 rounded-lg shadow dark:bg-gray-600 dark:border-gray-700 dark:text-white"
     >
       <form @submit.prevent="reading" class="">
         <h2 class="mb-5 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -71,7 +71,7 @@
             <option :value="16"></option>
           </datalist>
         </div>
-        <div class="flex space-x-4 pt-2">
+        <div class="flex-col lg:space-x-4 md:pt-2">
           <button
             type="submit"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
